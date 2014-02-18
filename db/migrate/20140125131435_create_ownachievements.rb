5287f421db2406a15eb35a8f1eafa663b4d1130b
FirstKuNGz <Firstrap7@gmail.com>
2014-02-18 12:07:08 +0700

    fix

--- db/migrate/20140125131435_create_ownachievements.rb
+++
@@ -1,9 +0,0 @@
-class CreateOwnachievements < ActiveRecord::Migration
-  def change
-    create_table :ownachievements ,:id => false do |t|
-		t.belongs_to :user
-		t.belongs_to :achievement		
-    end
-
-  end
-end
